90aa22dd20ac795f2536b1f1172e9caae27679ef
Michael <michael.loukeris@icloud.com>
2016-06-08 15:06:52 +0300

    Simple Commit

--- person.rb
+++ person.rb
@@ -2,18 +2,18 @@
 
 class Person
 	attr_accessor :name, :age
-	
+
 	def initialize(name, age)
 		@name = name
 		@age = age
 		end
-		
+
 	def print_person
 		puts "#{name} is #{age} years old"
 		end
 end
 
-
+#end
 
 
 #end
